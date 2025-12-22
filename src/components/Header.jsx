de002e67b167d9d5a78f82b0359c9ceefa769cbb
Nelson S. Ferrer IT-HO <nsferrer@puregold.local>
2025-12-22 10:32:45 +0800

    navigation enhancement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,108 @@
 'use client';
 import Link from 'next/link'
 import React, { useState } from 'react'
-import MainNav from './main-nav';
-import MobileNav from './mobile-nav';
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+
+const navigation = [
+    {name: 'home', href: '/'},
+    {name: 'about', href: '/about'},
+    {name: 'products', href: '/products'},
+    {name: 'contact', href: '/contact'},
+] 
 
 
 function Header() {
 
-    // const [isOpen, setIsOpen] = useState(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
-  return (
+return (
     
-    <header className='sticky top-0 w-full border-b flex py-6 px-2'>
-        <nav className='mx-auto flex items-center justify-between container'>
-            <div className='flex lg:flex-1 px-2 md:px-0'>
-                <Link href='/'>Property Listing</Link>
-            </div>
+    <div className="bg-gray-900">
+        <header className="mx-auto container inset-x-0 top-0 z-50 ">
+            
+            {/* DESKTOP */}
+            <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8 uppercase">
+                <div className="flex lg:flex-1">
+                    <a href="/" className="-m-1.5 p-1.5">
+                    <span className="sr-only">Property Listing</span>
+                    <span className="h-8 w-auto">Property Listing</span>
+                    {/* <img
+                        alt=""
+                        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                        className="h-8 w-auto"
+                    /> */}
+                    </a>
+                </div>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        onClick={() => setMobileMenuOpen(true)}
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-200"
+                    >
+                    <span className="sr-only">Open main menu</span>
+                    <Bars3Icon aria-hidden="true" className="size-6" />
+                    </button>
+                </div>
+                <div className="hidden lg:flex lg:gap-x-12">
+                    {navigation.map((item) => (
+                        <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-white">
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end text-xs">
+                    <a href="/log-in" className="text-sm/6 text-white">
+                        log in
+                    </a>
+                </div>
+            </nav>
 
-            {/* Desktop */}
-            <MainNav />
-            
-            {/* Mobile */}
-            <MobileNav />
-            
-            {/* Both View */}
-            <div className='hidden lg:flex lg:flex-1 lg:justify-end uppercase text-xs'>
-                <Link href='login' className='ps-3 pe-3 hover:text-gray-500'>log in</Link>
-            </div>
-        </nav>
-    </header>
+            {/* MOBILE */}
+            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+                <div className="fixed inset-0 z-50" />
+                <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
+                    <div className="flex items-center justify-between uppercase">
+                        <a href="/" className="-m-1.5 p-1.5">
+                            <span className="sr-only">Property Listing</span>
+                            <span className="h-8 w-auto">Property Listing</span>
+                            {/* <img
+                                alt=""
+                                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                                className="h-8 w-auto"
+                            /> */}
+                        </a>
+                        <button type="button" onClick={() => setMobileMenuOpen(false)} className="-m-2.5 rounded-md p-2.5 text-gray-200">
+                            <span className="sr-only">close menu</span>
+                            <XMarkIcon aria-hidden="true" className="size-6" />
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root uppercase">
+                        <div className="-my-6 divide-y divide-white/10">
+                            <div className="space-y-2 py-6">
+                                {navigation.map((item) => (
+                                    <Link
+                                        key={item.name}
+                                        href={item.href}
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                                        onClick={() => setMobileMenuOpen(false)}
+                                        >
+                                        {item.name}
+                                    </Link>
+                                ))}
+                            </div>
+                            <div className="py-6">
+                                <a href="/log-in" className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-white/5">
+                                    log in
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </DialogPanel>
+            </Dialog>
+        </header>
+    </div>
   )
 }
 
--- src/components/main-nav.jsx
+++ src/components/main-nav.jsx
@@ -3,10 +3,20 @@ import React from 'react'
 
 function MainNav() {
   return (
-    <div className='hidden lg:flex lg:gap-x-12'>
+    <div className='hidden lg:flex lg:gap-x-5'>
         <Link rel="stylesheet" href="/" className='mx-2 hover:text-gray-300'>Home</Link>
         <Link rel="stylesheet" href="/about" className='mx-2 hover:text-gray-300'>About</Link>
         <Link rel="stylesheet" href="/contact" className='mx-2 hover:text-gray-300'>Contact</Link>
+
+        {navigation.map((item) => (
+        <a
+            key={item.name}
+            href={item.href}
+            className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+        >
+            {item.name}
+        </a>
+        ))}
     </div>
   )
 }

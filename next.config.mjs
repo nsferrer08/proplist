6e80e749984f5de85a44656aa5d47dd52a49c787
nsferrer08 <56856765+nsferrer08@users.noreply.github.com>
2025-12-23 14:29:56 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,13 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  
+  // next.config.js
+  module.exports = {
+    output: 'export',
+    basePath: '/nxtjs-proplist',
+    trailingSlash: true, // Recommended for GitHub Pages
+  };
 };
-
-// next.config.js
-module.exports = {
-  output: 'export',
-  basePath: '/nxtjs-proplist',
-  trailingSlash: true, // Recommended for GitHub Pages
-};
-
 
 export default nextConfig;

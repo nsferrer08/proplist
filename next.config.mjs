d170386b5491cabd6419418105ac08e59d7b5f5a
Nelson S. Ferrer IT-HO <nsferrer@puregold.local>
2025-12-29 11:36:23 +0800

    seting-up base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  // Use the repo name for production, empty string for local
+  basePath: isProd ? '/proplist' : '',
+  // Optional: assetPrefix is often needed for images/CSS to load
+  assetPrefix: isProd ? '/proplist/' : '',
+  output: 'export', // Required for GitHub Pages
+  images: {
+    unoptimized: true, // Required for GitHub Pages
+  },
+
 };
 
 export default nextConfig;
